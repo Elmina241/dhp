1745b056c14e81df1ccfa028fd163f31f82a7ea6
Elmina <ElminaRoss@gmail.com>
2018-12-21 16:56:39 +1000

    base

--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+class Product_model(models.Model):
+    name = models.CharField(max_length=200)
+    group = models.ForeignKey('Model_group')
+    def __str__(self):
+        return self.name
+
+
+class Model_unit(models.Model):
+    model = models.ForeignKey('Product_model')
+    unit = models.ForeignKey('tables.Unit')
+    coeff = models.FloatField()
+    is_base = models.BooleanField()
+    def __str__(self):
+        return self.unit.name
+
+class Model_group(models.Model):
+    name = models.CharField(max_length=200)
+    parent = models.ForeignKey('Model_group', null = True)
+    def __str__(self):
+        return self.name
+
+class Property(models.Model):
+    name = models.CharField(max_length=200)
+    prop_type = models.PositiveSmallIntegerField()
+    def __str__(self):
+        return self.name
+
+class Property_range(Property):
+    inf = models.FloatField()
+    sup = models.FloatField()
+    def __str__(self):
+        return self.name
+
+class Property_var(models.Model):
+    prop = models.ForeignKey('Property')
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
+class Model_property(models.Model):
+    model = models.ForeignKey('Product_model')
+    prop = models.ForeignKey('Property')
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
+
+
+
