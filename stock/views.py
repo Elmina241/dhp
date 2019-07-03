7ebf030dc1aec73b044d9c1053752a01ec785f53
Elmina <ElminaRoss@gmail.com>
2019-07-03 17:20:06 +1000

    menu

--- stock/views.py
+++ stock/views.py
@@ -79,7 +79,7 @@ def inventory(request):
 
 def auth(request):
     users = User.objects.all()
-    if request.user.is_authenticated:
+    if request.user.is_authenticated():
         return redirect('main')
     else:
         return render(request, "login.html", {"users": users})
